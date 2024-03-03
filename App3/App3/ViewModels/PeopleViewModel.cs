6f54cae8ad7d53a0065cc80f9b46abbc79639c09
Matetorture <przemek.pytlarz18@gmail.com>
2024-03-03 17:08:47 +0100

    add project

---
+++ App3/App3/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using App3.Models;
+using System.Collections.ObjectModel;
+
+namespace App3.ViewModels
+{
+    public class PeopleViewModel
+    {
+        public ObservableCollection<Person> People { get; set; }
+
+        public PeopleViewModel()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person { Name = "Jan", Age = 20 },
+                new Person { Name = "Anna", Age = 25 },
+                new Person { Name = "Tomasz", Age = 30 }
+            };
+        }
+    }
+}
